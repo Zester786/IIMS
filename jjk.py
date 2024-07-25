b250af9eadb2c54afa79668eb7b4a213c20cd721
Zester786 <lucifertopden14@gmail.com>
2024-07-25 12:19:41 +0545

    Add files via upload

---
+++ jjk.py
@@ -0,0 +1,26 @@
+##Str1 = ("Hello " + "World")
+##print (Str1)
+
+##Str1 = "This is the one"
+##print (Str1.find("This"))
+
+##Str1 = "This is IIMS College."
+##vowels = "aeiouAEIOU"
+##vowel_count = {}
+####count = sum(Str1.count(vowels) for vowels in vowels)
+##for vowels in vowels:
+##    vowel_count[vowels] = Str1.count(vowels)
+##print (vowel_count)
+
+##Str1 = "this is shet and i love it"
+##print (Str1.upper())
+
+Str = "Apple, Ball, Cat, Dog, Egg".split(",")
+Str[::-1]
+print (Str[::-1])
+for s in Str:
+   print (s[::-1])
+
+
+
+
